9ca1e1622c154f92d553b0ae833826ed58ddc57a
nick_xyc <33674571+nickxyc@users.noreply.github.com>
2018-12-07 15:15:53 +0800

    修改部分bug

--- car.py
+++ car.py
@@ -13,28 +13,6 @@ auther nick
 '''
 caraction = Car()
 dangerous = dangerous()
-try:
-    '''设置树莓派上GPIO接口的初始状态，并且实例对象'''
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(caraction.IN3,GPIO.OUT,initial = GPIO.LOW)
-    GPIO.setup(caraction.IN4,GPIO.OUT,initial = GPIO.LOW)
-    GPIO.setup(caraction.IN1,GPIO.OUT,initial = GPIO.LOW)
-    GPIO.setup(caraction.IN2,GPIO.OUT,initial = GPIO.LOW)
-    GPIO.setup(dangerous.TRIG,GPIO.OUT,initial = GPIO.LOW)
-    GPIO.setup(dangerous.ECHO,GPIO.IN)
-    GPIO.setup(dangerous.BEE,GPIO.OUT,initial = GPIO.HIGH)
-    GPIO.setup(caraction.R_LIGHT,GPIO.OUT,initial = GPIO.LOW)
-    GPIO.setup(caraction.G_LIGHT,GPIO.OUT,initial = GPIO.LOW)
-    GPIO.setup(caraction.B_LIGHT,GPIO.OUT,initial = GPIO.LOW)
-    print('选择模式')
-    print('1.遥控模式')
-    choice = input()
-    if choice == '1':
-        remote()
-finally:
-    time.sleep(1)
-    GPIO.cleanup()
-
 def remote():
     a = threading.Thread(target=dangerous.check)
     a.start()
@@ -62,3 +40,26 @@ def deal(sock,addr,caration):
         caration.stop()
     if recvmessage == 'B':
         caration.back()
+try:
+    '''设置树莓派上GPIO接口的初始状态，并且实例对象'''
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(caraction.IN3,GPIO.OUT,initial = GPIO.LOW)
+    GPIO.setup(caraction.IN4,GPIO.OUT,initial = GPIO.LOW)
+    GPIO.setup(caraction.IN1,GPIO.OUT,initial = GPIO.LOW)
+    GPIO.setup(caraction.IN2,GPIO.OUT,initial = GPIO.LOW)
+    GPIO.setup(dangerous.TRIG,GPIO.OUT,initial = GPIO.LOW)
+    GPIO.setup(dangerous.ECHO,GPIO.IN)
+    GPIO.setup(dangerous.BEE,GPIO.OUT,initial = GPIO.HIGH)
+    GPIO.setup(caraction.R_LIGHT,GPIO.OUT,initial = GPIO.LOW)
+    GPIO.setup(caraction.G_LIGHT,GPIO.OUT,initial = GPIO.LOW)
+    GPIO.setup(caraction.B_LIGHT,GPIO.OUT,initial = GPIO.LOW)
+    print('选择模式')
+    print('1.遥控模式')
+    choice = input()
+    if choice == '1':
+        remote()
+finally:
+    time.sleep(1)
+    GPIO.cleanup()
+
+

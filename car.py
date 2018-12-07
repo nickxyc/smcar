6bca0e3969521eb72db04c6da8aac43eb7086fe9
nick_xyc <33674571+nickxyc@users.noreply.github.com>
2018-12-07 15:14:37 +0800

    修改部分bug

--- car.py
+++ car.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
-from remotecontrol import *
+#from remotecontrol import *
 from Carclass import Car
 import time
 from Carclass import dangerous
+import socket
+import threading
 '''小车的初始化程序，小车设备的主程序，各个模块的枢纽，也是面向用户的接口，用户可以通过这个模块来实现
 对小车的控制。未来友GUI计划，但是不保证可实现性'''
 '''
@@ -32,3 +34,31 @@ try:
 finally:
     time.sleep(1)
     GPIO.cleanup()
+
+def remote():
+    a = threading.Thread(target=dangerous.check)
+    a.start()
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind('127.0.0.1',9999)
+    s.listen(5)
+    print('等待连接')
+    while True:
+        sock,addr = s.accept()
+        d = threading.Thread(target=deal,args=(sock,addr,caration))
+        d.start()
+def deal(sock,addr,caration):
+    '''
+    处理来自网络的命令将其变成小车的动作
+    :return:
+    '''
+    recvmessage = sock.recv(1024).decode('utf-8')
+    if recvmessage == 'F':
+        caration.forward()
+    if recvmessage == 'L':
+        caration.turn_left()
+    if recvmessage == 'R':
+        caration.trun_right()
+    if recvmessage == 'S':
+        caration.stop()
+    if recvmessage == 'B':
+        caration.back()
